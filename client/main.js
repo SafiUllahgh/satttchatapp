203f649e7efd9dadc2361b4c42f8383f1722fcbd
unknown <you@example.com>
2023-01-15 22:42:14 +0500

    Initial Commit

--- client/main.js
+++ client/main.js
@@ -5,3 +5,10 @@ import App from './app'
 const rootNode = document.getElementById('react-container')
 
 ReactDOM.render(<App socket={io(window.location.host)} />, rootNode)
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
+
+
+
+ReactDOM.render(<App socket={io(window.location.host)} />, rootNode)
